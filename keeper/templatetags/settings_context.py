eba60420c94158f7e3eaed9047105a4c7a89adfb
jason-ellis <jason.ellis@unt.edu>
2019-09-19 09:54:12 -0500

    Add Google Analytics. Add new inherited CSS.

---
+++ keeper/templatetags/settings_context.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+
+def global_settings(request):
+    """Make settings available to templates.
+
+        defaults is a dictionary of settings to share with default values.
+        Attempt to get settings values from the imported settings.
+        If unavailable, use default values.
+        """
+    defaults = {'GA_TRACKING_ID': ''}
+    available_settings = {s: getattr(settings, s, defaults[s]) for s in defaults}
+    return available_settings
--- tests/settings/production.py
+++ tests/settings/production.py
@@ -1,3 +1,6 @@
 from .base import *
 
 DEBUG = False
+
+# Google Analytics
+GA_TRACKING_ID = 'UA-3629573-13'
