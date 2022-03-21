291c2000d897fa3571442d6d9f03711ac4245ee9
Jason Ellis <jason.ellis@unt.edu>
2022-03-21 14:13:19 -0500

    Refactor inherited template structure. Fix CRLF line endings to LF.

--- keeper/context_processor.py
+++ keeper/context_processor.py
@@ -1,44 +1,9 @@
+from django.conf import settings
+
 def context(request):
-    # stuff needed before defining context
     context = {
         'CANONICAL_PATH': request.build_absolute_uri(request.path),
-        'SOCIAL': [
-            {
-                'name': 'facebook',
-                'url': 'https://www.facebook.com/',
-                'id': 'unt.libraries',
-                'label': 'The UNT Libraries are on Facebook',
-                'icon': 'facebook-f',
-            },
-            {
-                'name': 'twitter',
-                'url': 'https://twitter.com/',
-                'id': 'unt_libraries',
-                'label': 'Follow the UNT Libraries on Twitter',
-                'icon': 'twitter',
-            },
-            {
-                'name': 'instagram',
-                'url': 'https://www.instagram.com/',
-                'id': 'unt_libraries',
-                'label': '"UNT Libraries on Instagram',
-                'icon': 'instagram',
-            },
-            {
-                'name': 'github',
-                'url': 'https://github.com/',
-                'id': 'unt-libraries',
-                'label': 'UNT Libraries on Github',
-                'icon': 'github-alt',
-            },
-            {
-                'name': 'youtube',
-                'url': 'https://www.youtube.com/channel/',
-                'id': 'UCcbS9fXWKE1SpUTpl1sNB6Q',
-                'label': 'UNT Libraries Youtube Channel',
-                'icon': 'youtube',
-            },
-        ]
+        'BRANDING': settings.BRANDING,
     }
 
     return context
