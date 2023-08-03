844bbca6e5b6f53ac9577c0174abb564e15ced4d
Jason Ellis <jason.ellis@unt.edu>
2023-08-03 15:43:38 -0500

    Add view tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,6 +1,9 @@
 import pytest
+from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
+
+from keeper.models import Accession, File
 
 
 @pytest.fixture
@@ -123,6 +126,40 @@ def test_submit_view_mixed_files(client, valid_accession_data, valid_file_data, 
 
 
 @pytest.mark.django_db(transaction=True)
+def test_submit_view_with_multiple_valid_files(client, valid_accession_data, valid_file_data):
+    content = b"file_content"
+    file1 = SimpleUploadedFile("file1.txt", content, content_type="text/plain")
+    file2 = SimpleUploadedFile("file2.txt", content, content_type="text/plain")
+
+    multiple_valid_files_data = {
+        'file-file': [file1, file2],
+        'file-file_description': ['Test file description 1', 'Test file description 2'],
+    }
+    post_data = {**valid_accession_data, **multiple_valid_files_data}
+
+    url = reverse('keeper:submit')
+    response = client.post(url, data=post_data)
+    assert response.status_code == 200
+    json_data = response.json()
+
+    # Check for success
+    assert json_data['success'] is True, f"Unexpected failure: {json_data.get('errors', 'Unknown error')}"
+
+    # Check that the correct number of file records were created
+    files = File.objects.filter(file_description__in=['Test file description 1', 'Test file description 2'])
+    assert files.count() == 2
+
+    expected_content = valid_file_data['file-file'].read()
+
+    # Check that the files have the expected content, etc.
+    for file in files:
+        assert file.get_filename() in ["file1.txt", "file2.txt"], "Unexpected file name"
+        assert file.file.read() == content, "File content does not match"
+        with open(file.file.path, 'rb') as f:
+            assert f.read() == expected_content
+
+
+@pytest.mark.django_db(transaction=True)
 def test_submit_view_session_data(client, valid_accession_data, valid_file_data):
     post_data = {**valid_accession_data, **valid_file_data}
 
@@ -156,7 +193,7 @@ def test_submit_view_with_empty_file(client, valid_accession_data):
 
 @pytest.mark.django_db(transaction=True)
 def test_submit_view_with_large_file(client, valid_accession_data):
-    large_file_content = b"A" * (64 * 1024 * 1024 + 1)  # Just over 64 MB
+    large_file_content = b"A" * (settings.MAX_UPLOAD_SIZE + 1)  # Max Upload is overridden in settings.test.py
     large_file = SimpleUploadedFile("large.txt", large_file_content, content_type="text/plain")
     post_data = {**valid_accession_data, 'file-file': large_file, 'file-file_description': 'Large file'}
     url = reverse('keeper:submit')
@@ -168,16 +205,29 @@ def test_submit_view_with_large_file(client, valid_accession_data):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_submit_view_with_invalid_content_file(client, valid_accession_data):
-    invalid_image_content = b"Not an image content"
-    invalid_image_file = SimpleUploadedFile("image.png", invalid_image_content, content_type="image/png")
-    post_data = {**valid_accession_data, 'file-file': invalid_image_file, 'file-file_description': 'Invalid content file'}
+def test_special_characters_in_input_fields(client, valid_file_data):
+    special_characters_data = {
+        'accession-first_name': 'John\'"; DROP TABLE --',
+        'accession-last_name': 'Doe',
+        'accession-email_address': 'johndoe@example.com',
+        'accession-phone_number': '123456789',
+        'accession-description': 'Test description with special characters: !@#$%^&*()',
+        'accession-affiliation': 'STU',
+    }
+    post_data = {**special_characters_data, **valid_file_data}
+
     url = reverse('keeper:submit')
     response = client.post(url, data=post_data)
     assert response.status_code == 200
     json_data = response.json()
-    assert json_data['success'] is False
-    assert 'errorsFile' in json_data
+    assert json_data['success'] is True
+
+    # Check that the special characters were stored correctly in the database
+    accession = Accession.objects.get(email_address='johndoe@example.com')
+    assert accession.first_name == special_characters_data['accession-first_name']
+
+    # Check that no unexpected error messages are present in the response
+    assert 'error' not in json_data
 
 
 @pytest.mark.django_db(transaction=True)
