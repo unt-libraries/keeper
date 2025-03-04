3ded083a06de279ebd6b38e483c6931e731ae2e1
Jason Ellis <jason.ellis@unt.edu>
2025-03-04 13:37:39 -0600

    Add yaml loader, fix omni relative urls, delete social links template

--- keeper/context_processor.py
+++ keeper/context_processor.py
@@ -1,9 +1,20 @@
+import os
+import yaml
 from django.conf import settings
 
-def context(request):
-    context = {
-        'CANONICAL_PATH': request.build_absolute_uri(request.path),
-        'BRANDING': settings.BRANDING,
-    }
+def load_yaml_data(directory):
+    data_dir = os.path.join(settings.BASE_DIR, directory)
+    data = {}
+    for filename in os.listdir(data_dir):
+        if filename.endswith('.yaml') or filename.endswith('.yml'):
+            file_path = os.path.join(data_dir, filename)
+            with open(file_path, 'r') as file:
+                key = os.path.splitext(filename)[0]
+                data[key] = yaml.safe_load(file)
+    return data
 
-    return context
+def context(request):
+    return {
+        'CANONICAL_PATH': request.build_absolute_uri(request.path),
+        'omni': load_yaml_data('data/omni'),
+    }
